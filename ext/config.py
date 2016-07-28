0f1a3a8bc81292aca08230c43bf938364e2beac8
Paul Gower <pgower@stmonicas.qld.edu.au>
2016-07-28 11:10:50 +1000

    Commiting before I delete locally.

--- ext/config.py
+++ ext/config.py
@@ -6,7 +6,19 @@ from cerberus import Validator
 logger = logging.getLogger(__name__)
 
 class DEConfigValidator(Validator):
-    def _validate_defined(self, defined, field, value):
+    def _validate_definedinput(self, defined, field, value):
         pass
 
 
+def validate(config_path):
+    try:
+        config = Configuration.configure(config_path)
+    except ConfigurationError as ce:
+        logger.exception(ce)
+    try:
+        with open('config.schema.yaml', 'r') as f:
+            schema = yaml.load(f)
+    except:
+        
+    schema = yaml.load('')
+    v = DEConfigValidator
--- plugins/input/sql.py
+++
@@ -1,3 +0,0 @@
-
-class MSSQLInputPlugin():
-    pass
